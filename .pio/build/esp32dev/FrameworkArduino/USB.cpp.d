4614ef36dfdfc064336e86ab12ae451f311d0aa7
jay <jay.steele2003@gmail.com>
2025-12-07 21:16:01 +0000

    platfromIO env best for esp32 development

---
+++ .pio/build/esp32dev/FrameworkArduino/USB.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
+ /home/jay/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
+ /home/jay/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/jay/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ .pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ /home/jay/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /home/jay/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/jay/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
